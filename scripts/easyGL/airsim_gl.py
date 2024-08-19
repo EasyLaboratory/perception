2821a6de3e53a9d8d88de0d14138c957181f907b
wuyu <bupt_wuyu@163.com>
2024-08-19 12:24:31 +0800

    add noise

--- scripts/easyGL/airsim_gl.py
+++ scripts/easyGL/airsim_gl.py
@@ -78,6 +78,28 @@ def get_annotated_image_center(results:List[ultralytics.engine.results.Results],
     return annotated_image
 
 
+def add_noise(depth_map, theta_y):
+    """
+    根据已知噪声模型对超出理想范围的深度图进行去噪处理
+    :param depth_map: 原始深度图，单位为米，numpy数组
+    :param theta_y: 入射角度，单位为弧度，numpy数组，与depth_map形状相同
+    :return: 去噪后的深度图
+    """
+    # 噪声模型参数
+    a0 = 0.001063
+    a1 = 0.0007278
+    a2 = 0.003949
+    b = 0.022
+    
+    # 计算距离z
+    z = depth_map
+    
+    # 计算轴向噪声 σz(z, θy)
+    axial_noise_sigma = (a0 + a1*z + a2*z**2) + (b * z**(3/2)) / (theta_y * (np.pi/2 - theta_y)**2)
+
+    noise = np.random.normal(0,axial_noise_sigma)
+    return depth_map+noise
+
 
 
     
